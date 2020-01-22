9ea3ec40c43c0938ff5adbecb2ca561409876313
Alonso Bermejo <59649469+CLiPeStar@users.noreply.github.com>
2020-01-22 13:30:52 +0100

    Clase de areasAlonso con area del triangulo completa

--- practicaEntono/src/practicaEntornoJ/A/F/areasAlonso.java
+++ practicaEntono/src/practicaEntornoJ/A/F/areasAlonso.java
@@ -1,10 +1,14 @@
 package practicaEntornoJ.A.F;
 
 public class areasAlonso {
-public int areaTriangulo() {
-	int b=
-	int area=(b*a)/2;
+	public static int areaTriangulo() {
+		System.out.println("Necesito la base");
+		int b = Leer.entero();
+		System.out.println("Necesito la altura");
+		int a = Leer.entero();
+		int area = (b * a) / 2;
+
+		return area;
+	}
 	
-	return -1;
-}
 }
