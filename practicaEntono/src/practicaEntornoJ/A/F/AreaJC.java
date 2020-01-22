2b6ac2fbd6f699df1d5f14ddf267974151536ffd
Juanca <juancdmh@gmail.com>
2020-01-22 13:43:40 +0100

    h

---
+++ practicaEntono/src/practicaEntornoJ/A/F/AreaJC.java
@@ -0,0 +1,19 @@
+package practicaEntornoJ.A.F;
+
+public class AreaJC {
+
+	public static double areaCuadrilateroConvexo() {
+		double a = Leer.decimal();
+		double b = Leer.decimal();
+		double angulo = Leer.decimal();
+		double resultado = 0.5*a*b*(Math.toDegrees(Math.sin(angulo)));
+		
+		return resultado;
+	}
+	
+	public static double areaCirculo() {
+		double r = Leer.decimal();
+		double area = Math.PI*Math.pow(Math.PI, r);
+		return area;
+	}
+}
