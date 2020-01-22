378e8310f26f17efc21b1df66a2c99d33d9d5701
felixJarteaga <59649556+felixJarteaga@users.noreply.github.com>
2020-01-22 13:57:23 +0100

    Add files via upload

---
+++ areaFelix.java
@@ -0,0 +1,14 @@
+package practicaEntornoJ.A.F;
+
+public class areaFelix {
+
+	public int areaParalelogramo() {
+		System.out.println("Introduce la altura");
+		int h=Leer.entero();
+		System.out.println("Introduce la base mayor");
+		int a=Leer.entero();
+		int area=a*h;
+		return area;
+	
+	}
+}
