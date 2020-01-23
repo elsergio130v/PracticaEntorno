83f4f16c9a780c9259b68669d9e675a79e5f8306
Juanca <juancdmh@gmail.com>
2020-01-23 08:40:09 +0100

    Sysos introducidos

--- practicaEntono/src/practicaEntornoJ/A/F/AreaJC.java
+++ practicaEntono/src/practicaEntornoJ/A/F/AreaJC.java
@@ -3,8 +3,11 @@ package practicaEntornoJ.A.F;
 public class AreaJC {
 
 	public static double areaCuadrilateroConvexo() {
+		System.out.println("Introduce la primera diagonal");
 		double a = Leer.decimal();
+		System.out.println("Introduce la segunda diagonal");
 		double b = Leer.decimal();
+		System.out.println("Introduce el angulo de las diagonales");
 		double angulo = Leer.decimal();
 		double resultado = 0.5*a*b*(Math.toDegrees(Math.sin(angulo)));
 		
@@ -12,13 +15,16 @@ public class AreaJC {
 	}
 	
 	public static double areaCirculo() {
+		System.out.println("Introduce el radio");
 		double r = Leer.decimal();
 		double area = Math.PI*Math.pow(Math.PI, r);
 		return area;
 	}
 	
 	public static double areaElipse() {
+		System.out.println("Introduce el primer Semieje");
 		double a = Leer.decimal();
+		System.out.println("Introduce el segundo Semieje");
 		double b = Leer.decimal();
 		double area = Math.PI*a*b;
 		return area;
