e02fe05a6753cece81c1ca0462135cc065e6bbf3
felixJarteaga <59649556+felixJarteaga@users.noreply.github.com>
2020-01-28 13:57:17 +0100

    este es el area del rombo

--- practicaEntono/src/practicaEntornoJ/A/F/areaFelix.java
+++ practicaEntono/src/practicaEntornoJ/A/F/areaFelix.java
@@ -11,4 +11,24 @@ public class areaFelix {
 		return area;
 		
 	}
+	
+	public int areaRombo() {
+		System.out.println("Introduce la altura");
+		int h=Leer.entero();
+		System.out.println("Introduce el lado del rombo");
+		int a=Leer.entero();
+		int area=a*h;
+		return area;	
+	}
+	public int areaTrapecio() {
+		System.out.println("Introduce la altura");
+		int h=Leer.entero();
+		System.out.println("Introduce la base menor");
+		int a=Leer.entero();
+		System.out.println("Introduce la base mayor");
+		int b=Leer.entero();
+		int area=((a+b)/2)*h;
+		return area;	
+		
+	}
 }
