f91d33340f2af38061a44dafa2593a13bf37016c
Alonso Bermejo <59649469+CLiPeStar@users.noreply.github.com>
2020-01-28 13:30:44 +0100

    area rectangulo subida

--- practicaEntono/src/practicaEntornoJ/A/F/areasAlonso.java
+++ practicaEntono/src/practicaEntornoJ/A/F/areasAlonso.java
@@ -10,12 +10,21 @@ public class areasAlonso {
 
 		return area;
 	}
-	
-	
+
 	public int areaCuadrado() {
 		System.out.println("Necesito el lado");
 		int lado = Leer.entero();
-		int area=lado^2;
+		int area = lado ^ 2;
 		return area;
 	}
+
+	public int areaRectangulo() {
+		System.out.println("Necesito el lado");
+		int b = Leer.entero();
+		System.out.println("Necesito la base");
+		int a = Leer.entero();
+		int area = b * a;
+		return area;
+	}
+
 }
