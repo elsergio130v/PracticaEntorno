42c0f6ae5faa0da5232c73d93665fbfbfd341b34
Alonso Bermejo <59649469+CLiPeStar@users.noreply.github.com>
2020-01-29 13:48:46 +0100

    Update areaFelix.java

--- practicaEntono/src/practicaEntornoJ/A/F/areaFelix.java
+++ practicaEntono/src/practicaEntornoJ/A/F/areaFelix.java
@@ -26,16 +26,6 @@ public class areaFelix {
 	}
 	
 	
-	
-	
-	public static void areaRombo() {
-		System.out.println("Introduce la altura");
-		int h=Leer.entero();
-		System.out.println("Introduce el lado del rombo");
-		int a=Leer.entero();
-		int area=a*h;
-		System.out.println("El area del rombo es "+area);
-	}
 	public static void areaTrapecio() {
 		System.out.println("Introduce la altura");
 		int h=Leer.entero();
