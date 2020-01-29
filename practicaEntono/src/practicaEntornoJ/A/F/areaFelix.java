e94c556d3b3892b4fa76a1ab93d83e6bb1c9ea9d
Alemew <59649239+Alemew@users.noreply.github.com>
2020-01-29 13:46:25 +0100

    Cambio AreaRombo
    El area del rombo lo teneis mal os la he cambiado

--- practicaEntono/src/practicaEntornoJ/A/F/areaFelix.java
+++ practicaEntono/src/practicaEntornoJ/A/F/areaFelix.java
@@ -11,6 +11,23 @@ public class areaFelix {
 	System.out.println("El area del paralelogramo es "+area);
 	}
 	
+	//En el area del rombo en internet sale que es la multiplicacion de las diagonales dividido entre 2
+	//Creo que el area que habeis puesto no es el area del rombo si no mas bien del cuadrado,
+	//Estas dos se suelen confundir pero no son iguales, por eso yo usaria esto.
+	
+	public static void areaRombo() {
+		System.out.println("Introduce una Diagonal")
+		int diagonalUno=Leer.entero();
+		System.out.println("Introduce otra Diagonal")
+		int diagonaldos=Leer.entero();
+		int area=(diagonalUno*diagonalDos)/2
+		System.out.println("El area del rombo es"+area)
+		
+	}
+	
+	
+	
+	
 	public static void areaRombo() {
 		System.out.println("Introduce la altura");
 		int h=Leer.entero();
